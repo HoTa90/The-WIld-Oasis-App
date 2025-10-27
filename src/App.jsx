5b67614a193b1aac03cda97daa68a7fda646d85c
Aleksander Dimitrov <alexander.dimitrov90@gmail.com>
2025-10-27 14:46:24 +0200

    added styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
+import styled from "styled-components";
 
+const H1 = styled.h1`
+	font-size: 30px;
+	font-weight: 600;
+	background-color: blue;
+`;
 
 function App() {
- 
-
-  return (
-    <>
-     
-    </>
-  )
+	return <H1>Hello</H1>;
 }
 
-export default App
+export default App;
