df54b0c31dd3f30d95188a6005662f487e29864a
Aleksander Dimitrov <alexander.dimitrov90@gmail.com>
2025-11-04 16:39:03 +0200

    refactored table into compound component

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -1,24 +1,24 @@
 import styled from "styled-components";
 import { formatCurrency } from "../../utils/helpers.js";
-import { useState } from "react";
 import CreateCabinForm from "./CreateCabinForm.jsx";
 import { useDeleteCabin } from "./useDeleteCabin.js";
 import { HiPencil, HiSquare2Stack, HiTrash } from "react-icons/hi2";
 import { useCreateCabin } from "./useCreateCabin.js";
 import Modal from "../../ui/Modal.jsx";
 import ConfirmDelete from "../../ui/ConfirmDelete.jsx";
+import Table from "../../ui/Table.jsx";
 
-const TableRow = styled.div`
-	display: grid;
-	grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
-	column-gap: 2.4rem;
-	align-items: center;
-	padding: 1.4rem 2.4rem;
+// const TableRow = styled.div`
+// 	display: grid;
+// 	grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
+// 	column-gap: 2.4rem;
+// 	align-items: center;
+// 	padding: 1.4rem 2.4rem;
 
-	&:not(:last-child) {
-		border-bottom: 1px solid var(--color-grey-100);
-	}
-`;
+// 	&:not(:last-child) {
+// 		border-bottom: 1px solid var(--color-grey-100);
+// 	}
+// `;
 
 const Img = styled.img`
 	display: block;
@@ -65,7 +65,7 @@ function CabinRow({ cabin }) {
 	}
 
 	return (
-		<TableRow role="row">
+		<Table.Row>
 			<Img src={image} />
 			<Cabin>{name}</Cabin>
 			<div>Fits up to {max_capacity} guests</div>
@@ -87,12 +87,12 @@ function CabinRow({ cabin }) {
 						<CreateCabinForm cabinForEdit={cabin} />
 					</Modal.Window>
 
-					<Modal.Open>
+					<Modal.Open opens={"delete"}>
 						<button>
 							<HiTrash />
 						</button>
 					</Modal.Open>
-					<Modal.Window>
+					<Modal.Window name={"delete"}>
 						<ConfirmDelete
 							resourceName={"cabins"}
 							disabled={isPending}
@@ -101,7 +101,7 @@ function CabinRow({ cabin }) {
 					</Modal.Window>
 				</Modal>
 			</div>
-		</TableRow>
+		</Table.Row>
 	);
 }
 export default CabinRow;
