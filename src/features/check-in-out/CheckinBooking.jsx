4562de1a6ccfeb0578dfd7628b735473399a2717
Aleksander Dimitrov <alexander.dimitrov90@gmail.com>
2025-11-09 18:56:59 +0200

    added checkin option

--- src/features/check-in-out/CheckinBooking.jsx
+++ src/features/check-in-out/CheckinBooking.jsx
@@ -8,48 +8,68 @@ import Button from "../../ui/Button";
 import ButtonText from "../../ui/ButtonText";
 
 import { useMoveBack } from "../../hooks/useMoveBack";
+import { useBooking } from "../bookings/useBooking.js";
+import Spinner from "../../ui/Spinner.jsx";
+import { useEffect, useState } from "react";
+import Checkbox from "../../ui/Checkbox.jsx";
+import { formatCurrency } from "../../utils/helpers.js";
+import { useCheckin } from "./useCheckin.js";
 
 const Box = styled.div`
-  /* Box */
-  background-color: var(--color-grey-0);
-  border: 1px solid var(--color-grey-100);
-  border-radius: var(--border-radius-md);
-  padding: 2.4rem 4rem;
+	/* Box */
+	background-color: var(--color-grey-0);
+	border: 1px solid var(--color-grey-100);
+	border-radius: var(--border-radius-md);
+	padding: 2.4rem 4rem;
 `;
 
 function CheckinBooking() {
-  const moveBack = useMoveBack();
+	const [confirmedPaid, setConfirmedPaid] = useState(false);
+	const { booking, isPending } = useBooking();
 
-  const booking = {};
+	useEffect(() => {
+		setConfirmedPaid(booking?.has_paid ?? false);
+	}, [booking?.has_paid]);
+	const moveBack = useMoveBack();
+	const {checkin, isCheckingIn} = useCheckin();
 
-  const {
-    id: bookingId,
-    guests,
-    total_price,
-    num_guests,
-    has_breakfast,
-    num_nights,
-  } = booking;
+	if (isPending) return <Spinner />;
 
-  function handleCheckin() {}
+	const { id: bookingId, guests, total_price, num_guests, has_breakfast, num_nights } = booking;
 
-  return (
-    <>
-      <Row type="horizontal">
-        <Heading as="h1">Check in booking #{bookingId}</Heading>
-        <ButtonText onClick={moveBack}>&larr; Back</ButtonText>
-      </Row>
+	function handleCheckin() {
+		if (!confirmedPaid) return
+		checkin(bookingId)
+	}
 
-      <BookingDataBox booking={booking} />
+	return (
+		<>
+			<Row type="horizontal">
+				<Heading as="h1">Check in booking #{bookingId}</Heading>
+				<ButtonText onClick={moveBack}>&larr; Back</ButtonText>
+			</Row>
 
-      <ButtonGroup>
-        <Button onClick={handleCheckin}>Check in booking #{bookingId}</Button>
-        <Button variation="secondary" onClick={moveBack}>
-          Back
-        </Button>
-      </ButtonGroup>
-    </>
-  );
+			<BookingDataBox booking={booking} />
+			<Box>
+				<Checkbox
+					checked={confirmedPaid}
+					onChange={() => setConfirmedPaid((p) => !p)}
+					id={"confirm"}
+					disabled={confirmedPaid || isCheckingIn}>
+					confirm that {guests.full_name} has paid the total amount of {formatCurrency(total_price)}
+				</Checkbox>
+			</Box>
+
+			<ButtonGroup>
+				<Button onClick={handleCheckin} disabled={!confirmedPaid || isCheckingIn}>Check in booking #{bookingId}</Button>
+				<Button
+					$variation="secondary"
+					onClick={moveBack}>
+					Back
+				</Button>
+			</ButtonGroup>
+		</>
+	);
 }
 
 export default CheckinBooking;
---
+++ src/features/check-in-out/useCheckin.js
@@ -0,0 +1,22 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { updateBooking } from "../../services/apiBookings.js";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router";
+
+export function useCheckin() {
+	const queryClient = useQueryClient();
+	const navigate = useNavigate();
+	const { mutate: checkin, isPending: isCheckingIn } = useMutation({
+		mutationFn: (bookingId) => updateBooking(bookingId, { status: "checked-in", has_paid: true }),
+		onSuccess: (data) => {
+			toast.success(`Booking #${data.id} successfully checked in`);
+			queryClient.invalidateQueries({ active: true });
+			navigate("/");
+		},
+		onError: () => {
+			toast.error("There was an error while checking in");
+		},
+	});
+
+	return { checkin, isCheckingIn };
+}
